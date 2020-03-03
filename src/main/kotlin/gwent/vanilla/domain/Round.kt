85774fc1733cfbfb530cf36194f48f0b4f6ed218
cogitantium <falke.cs@gmail.com>
2020-03-03 13:08:18 +0100

    [backend] Refactor Pair to 'to'

--- src/main/kotlin/gwent/vanilla/domain/Round.kt
+++ src/main/kotlin/gwent/vanilla/domain/Round.kt
@@ -4,7 +4,7 @@ import gwent.vanilla.domain.Board
 import gwent.vanilla.domain.Player
 
 data class Round(val player1: Player, val player2: Player, val startingplayer: Player) {
-    var boards: Map<Player, Board> = mapOf(Pair(player1, Board()), Pair(player2, Board()))
+    var boards: Map<Player, Board> = mapOf(player1 to Board(), player2 to Board())
     var turn = 0
     lateinit var lastPlayer: Player
 
