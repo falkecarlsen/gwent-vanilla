ff9395dd72d0503e3ddd8bd7a1f69b13d0383623
cogitantium <falke.cs@gmail.com>
2020-03-04 12:48:55 +0100

    [backend] Add addCreature functionality

--- src/main/kotlin/gwent/vanilla/domain/Board.kt
+++ src/main/kotlin/gwent/vanilla/domain/Board.kt
@@ -1,7 +1,11 @@
 package gwent.vanilla.domain
 
-data class Board(var rows: List<Row> = mutableListOf(), var power: Int = 0) {
+data class Board(var rows: MutableMap<RowSuit, Row> = mutableMapOf(), var power: Int = 0) {
     fun calcPower() {
         TODO("not implemented")
+    }
+
+    fun addCard(creature: Creature, rowSuit: RowSuit) {
+        rows[rowSuit]!!.cards.add(creature)
     }
 }
