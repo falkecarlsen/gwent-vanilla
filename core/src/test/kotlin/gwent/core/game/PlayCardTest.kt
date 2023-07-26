362009160a20a8d0d6e35cb86cc5567609544141
NicEastvillage <nicoesterby@gmail.com>
2023-07-26 16:33:46 +0200

    [test] Add testing of wild/heart cards

--- core/src/test/kotlin/gwent/core/game/PlayCardTest.kt
+++ core/src/test/kotlin/gwent/core/game/PlayCardTest.kt
@@ -38,4 +38,45 @@ class PlayCardTest {
             game.tryPerformAction(action)
         }
     }
+
+    @Test
+    fun playCard03() {
+        // Check if playing a wild card works
+        val game = Game("Alice", "Bob", TestSetup.stackedDeck(p0Cards = listOf(Card.Hearts7)), 0)
+        val action = PlayCard(0, Card.Hearts7, RowSuit.DIAMONDS)
+
+        assert(game.players[0].hand.contains(Card.Hearts7))
+        assert(game.players[0].board.rows[RowSuit.DIAMONDS]!!.cards.isEmpty())
+
+        game.tryPerformAction(action)
+
+        assert(!game.players[0].hand.contains(Card.Hearts7))
+        assert(Card.Hearts7 in game.players[0].board.rows[RowSuit.DIAMONDS]!!.cards)
+        assert(game.players[0].board.rows[RowSuit.CLUBS]!!.cards.isEmpty())
+        assert(game.players[0].board.rows[RowSuit.SPADES]!!.cards.isEmpty())
+    }
+
+    @Test
+    fun playCard04() {
+        // Check if you can play a wild card without specifying row
+        val game = Game("Alice", "Bob", TestSetup.stackedDeck(p0Cards = listOf(Card.Hearts9)), 0)
+        val action = PlayCard(0, Card.Hearts9, null)
+
+        assert(game.players[0].hand.contains(Card.Hearts9))
+        assertFailsWith(MissingRowParameterException::class) {
+            game.tryPerformAction(action)
+        }
+    }
+
+    @Test
+    fun playCard05() {
+        // Non-wild cards must be played in the row matching their suit
+        val game = Game("Alice", "Bob", TestSetup.stackedDeck(p0Cards = listOf(Card.Clubs4)), 0)
+        val action = PlayCard(0, Card.Clubs4, RowSuit.DIAMONDS)
+
+        assert(game.players[0].hand.contains(Card.Clubs4))
+        assertFailsWith(InvalidRowParameterException::class) {
+            game.tryPerformAction(action)
+        }
+    }
 }
